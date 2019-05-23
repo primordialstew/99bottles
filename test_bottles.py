d998f902ee9e459a7d07a09affdcb8f10bc844c1
Brian Stewart <bstewart@usnews.com>
2019-05-23 14:50:29 -0400

    [Red] Test another 'default' case

--- test_bottles.py
+++ test_bottles.py
@@ -3,7 +3,15 @@ from bottles import Bottles
 
 def test_the_first_verse():
     expected = (
-        "99 bottles of beer on the wall, 99 bottles of beer.\n",
+        "99 bottles of beer on the wall, 99 bottles of beer.\n"
         "Take one down and pass it around, 98 bottles of beer on the wall.\n"
     )
     assert Bottles().verse(99) == expected
+
+
+def test_another_verse():
+    expected = (
+        "3 bottles of beer on the wall, 3 bottles of beer.\n"
+        "Take one down and pass it around, 2 bottles of beer on the wall.\n"
+    )
+    assert Bottles().verse(3) == expected
