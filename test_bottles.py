f0cc1d5b93951d28c570d1430835a63fb24bcd7a
Brian Stewart <bstewart@usnews.com>
2019-05-23 18:13:16 -0400

    [Red] Test 1-bottle verse

--- test_bottles.py
+++ test_bottles.py
@@ -17,9 +17,19 @@ def test_another_verse():
     assert Bottles().verse(3) == expected
 
 
-def test_verse_2():
+def test_verse_with_2_bottles():
     expected = (
         "2 bottles of beer on the wall, 2 bottles of beer.\n"
         "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     )
     assert Bottles().verse(2) == expected
+
+
+def test_verse_with_1_bottle():
+
+    expected = (
+        "1 bottle of beer on the wall, 1 bottle of beer.\n"
+        "Take it down and pass it around, "
+        "no more bottles of beer on the wall.\n"
+    )
+    assert Bottles().verse(1) == expected
