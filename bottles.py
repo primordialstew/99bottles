c0d44aa7f6adedbba116af73a9322bdbf6ee5e83
Brian Stewart <bstewart@usnews.com>
2019-05-23 17:55:03 -0400

    Handle 2-bottles case

--- bottles.py
+++ bottles.py
@@ -2,10 +2,18 @@ class Bottles(object):
 
     @staticmethod
     def verse(number):
-        verse = (
-            "{n} bottles of beer on the wall, "
-            "{n} bottles of beer.\n"
-            "Take one down and pass it around, "
-            "{m} bottles of beer on the wall.\n"
-        ).format(n=number, m=number-1)
+        if number == 2:
+            verse = (
+                "2 bottles of beer on the wall, "
+                "2 bottles of beer.\n"
+                "Take one down and pass it around, "
+                "1 bottle of beer on the wall.\n"
+            )
+        else:
+            verse = (
+                "{n} bottles of beer on the wall, "
+                "{n} bottles of beer.\n"
+                "Take one down and pass it around, "
+                "{m} bottles of beer on the wall.\n"
+            ).format(n=number, m=number-1)
         return verse
